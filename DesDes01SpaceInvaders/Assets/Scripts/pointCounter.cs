27fcba7437dbeabf534324db136333bc7d381fcc
Floppsopp <lukasjoneklint@gmail.com>
2025-05-07 10:03:25 +0200

    score counter

---
+++ DesDes01SpaceInvaders/Assets/Scripts/pointCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointCounter : MonoBehaviour
+{
+    [SerializeField] PointHUD pointHUD;
+
+
+    private void Start()
+    {
+        StartCoroutine(CountPoints());
+    }
+    private IEnumerator CountPoints()
+    {
+        while(true)
+        {
+            pointHUD.Points += 5;
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
