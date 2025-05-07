a1413a83925732ad8e97590d6a111413080cb579
Floppsopp <lukasjoneklint@gmail.com>
2025-05-07 11:06:59 +0200

    new points

--- DesDes01SpaceInvaders/Assets/Scripts/Lazer.cs
+++ DesDes01SpaceInvaders/Assets/Scripts/Lazer.cs
@@ -14,6 +14,14 @@ public class Lazer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            FindObjectOfType<pointCounter>().AddPoints(enemy.score);
+            Destroy(collision.gameObject); // förstör fienden
+            Destroy(gameObject); // förstör kulan
+        }
+
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
